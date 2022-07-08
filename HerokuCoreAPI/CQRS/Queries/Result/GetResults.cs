85bae12f113e424e11052dde6a0087b8a5d98679
Elisa Niyonkuru <niyonkuruelisa@gmail.com>
2022-07-08 17:45:08 +0200

    Added Memory Cache to Project.

--- HerokuCoreAPI/CQRS/Queries/Result/GetResults.cs
+++ HerokuCoreAPI/CQRS/Queries/Result/GetResults.cs
@@ -1,30 +1,28 @@
-﻿using MediatR;
+﻿using HerokuCoreAPI.Data;
+using MediatR;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace HerokuCoreAPI.CQRS.Queries.Result
 {
     public class GetResults
     {
-        IMemoryCache MemoryCache { get; }
-        public GetResults(IMemoryCache MemoryCache)
-        {
-            this.MemoryCache = MemoryCache;
-        }
+
+        
         // Data to execute
         public record Query(string auth,string start,string end) : IRequest<Response>;
         // Handler
         public class Handler : IRequestHandler<Query, Response>
         {
+            private readonly IMemoryCache MemoryCache;
+            public Handler(IMemoryCache MemoryCache)
+            {
+                this.MemoryCache = MemoryCache;
+            }
             public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
             {
-                Console.WriteLine("Auth {0} start {1} end {2}", request.auth, request.start, request.end);
-                var rand = new Random();
-                var rtnlist = new List<int>();
-
-                for (int i = 0; i < 100; i++)
-                {
-                    rtnlist.Add(rand.Next(1000000));
-                }
+                
+                ResultsIDData results = new ResultsIDData();
+                List<int> rtnlist = await results.GetResultsIntsCache(MemoryCache);
 
                 return new Response(randomInts: rtnlist);
             }
---
+++ HerokuCoreAPI/Data/ResultsIDData.cs
@@ -0,0 +1,44 @@
+﻿
+using Microsoft.Extensions.Caching.Memory;
+namespace HerokuCoreAPI.Data{
+    public class ResultsIDData
+    {
+        public async Task<List<int>> GetResultsInts()
+        {
+            return new List<int>
+                {
+                    67518,
+                    120697,
+                    806613,
+                    762440,
+                    553141,
+                    988079,
+                    939136,
+                    583596,
+                    573055,
+                    939136,
+                    583596,
+                    573055,
+                };
+        }
+        public async Task<List<int>> GetResultsIntsCache(IMemoryCache memoryCashe)
+        {
+            List<int> output = new List<int>();
+            //check if there is a cache for the results
+            output = memoryCashe.Get<List<int>>("ResultsInt");
+            if (output is null)
+            {
+                Console.WriteLine("New Data...");
+
+                // if no caches return data then catch data.
+                output = await GetResultsInts();
+                memoryCashe.Set("ResultsInt", output, TimeSpan.FromMinutes(5));
+            }
+            else
+            {
+                Console.WriteLine("Cached Data...");
+            }
+            return output;
+        }
+    }
+}
